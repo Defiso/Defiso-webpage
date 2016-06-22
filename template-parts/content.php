1c29c01f42f5770ef9df46b396633b42ff711df9
Anton Kjörck Lindén <anton@noviumdesign.se>
2016-06-22 16:07:30 +0200

    Fixes for blog

--- template-parts/content.php
+++ template-parts/content.php
@@ -17,33 +17,46 @@
 			} else {
 				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 			}
-
-		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php defisomedia_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php
-		endif; ?>
+		?>
 	</header><!-- .entry-header -->
 
-	<div class="entry-content">
-		<?php
-			if ( has_post_thumbnail() ) {
-				echo the_post_thumbnail_url();
-			}
+		<?php if ( is_single() ) {
+			echo '<div class="blog-container">';
+		} ?>
+		
+		<?php if ( 'post' === get_post_type() ) : ?>
+			<div class="entry-meta">
+				<?php defisomedia_posted_on(); ?>
+			</div><!-- .entry-meta -->
+			<?php
+		endif; ?>
 
-			the_content( sprintf(
-				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'defisomedia' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
+		<div class="entry-content">
+			<?php
+				if ( has_post_thumbnail() ) {
+					echo the_post_thumbnail_url();
+				}
 
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'defisomedia' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
+				if ( is_single() ) {
+					the_content( sprintf(
+						/* translators: %s: Name of current post. */
+						wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'defisomedia' ), array( 'span' => array( 'class' => array() ) ) ),
+						the_title( '<span class="screen-reader-text">"', '"</span>', false )
+					) );
+				} else {
+					the_excerpt(); ?>
+					<a href="<?php echo esc_url( get_permalink() ) ?>" rel="bookmark">Läs mer</a>
+				<?php }
+
+				wp_link_pages( array(
+					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'defisomedia' ),
+					'after'  => '</div>',
+				) );
+			?>
+		</div><!-- .entry-content -->
+		<?php if ( is_single() ) {
+			echo '</div>';
+		} ?>
 
 	<footer class="entry-footer">
 		<?php defisomedia_entry_footer(); ?>
