7d945e3a701fa82fe090b0da86ce4ae45b89e77c
Samuel Lie <samuel.lie1@gmail.com>
2016-08-23 13:21:46 +0200

    Standard template update with columns

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -14,6 +14,28 @@
 		<?php get_template_part( 'template-parts/elements/element', 'jumbotron' ); ?>
 	</header><!-- .entry-header -->
 
+	<?php if ('standard_tmpl_content_row') {
+		while ( have_rows('standard_tmpl_content_row') ) : the_row(); ?>
+			<div class="two-columns">
+				<div class="content">
+					<?php if (get_sub_field('row_title')): ?>
+						<div class="header">
+							<h2><?php the_sub_field('row_title') ?></h2>
+						</div>
+					<?php endif; ?>
+					<div class="columns">
+						<div class="column">
+							<?php the_sub_field('row_left_column') ?>
+						</div>
+						<div class="column">
+							<?php the_sub_field('row_right_column') ?>
+						</div>
+					</div>
+				</div>
+			</div>
+		<?php endwhile;
+	} ?>
+
 	<div class="entry-content">
 		<?php
 			the_content();
