246d4ffe957f345090237bd8feb783e7f6834d86
Samuel Lie <samuel.lie1@gmail.com>
2016-08-24 14:49:48 +0200

    move modal script to template file

--- tmpl-seo-result.php
+++ tmpl-seo-result.php
@@ -78,7 +78,7 @@
 		  timeLeft.text('Vänta lite till');
 		}, 5000);
 
- $.ajax({
+	 $.ajax({
       method: "POST",
       url: "<?php echo get_template_directory_uri();?>/inc/seoTool/analyze.php",
       data: inputs,
@@ -93,5 +93,52 @@
       },
       complete: function (data) {}
     })
+
+		// SEO modal
+	  var triggeredModalOnce = false
+
+	  if (!triggeredModalOnce) {
+	    $(window).scroll(openModal);
+	  }
+
+	  function openModal() {
+	    console.log(triggeredModalOnce);
+	    if ( ($('#modal-opener').isOnScreen() == true ) && (!triggeredModalOnce) ) {
+	      $("body").addClass("modal-open");
+	      $('#seo-modal').prop('checked', true);
+	      triggeredModalOnce = true;
+	    }
+	  }
+
+	  $('#seo-modal').on('change', function() {
+	    if ($(this).is(':checked')) {
+	      $('body').addClass('modal-open');
+	    } else {
+	      $('body').removeClass('modal-open');
+	    }
+	  });
+
+	  $('.modal-fade-screen, .modal-close').on('click', function() {
+	    $('.modal-state:checked').prop('checked', false).change();
+	  });
+
+	  $('.modal-inner').on('click', function(e) {
+	    console.log('clickededd');
+	    e.stopPropagation();
+	  });
+
+	  $.fn.isOnScreen = function(){
+	    var win = $(window);
+	    var viewport = {
+	      top : win.scrollTop(),
+	      left : win.scrollLeft()
+	    };
+	    viewport.right = viewport.left + win.width();
+	    viewport.bottom = viewport.top + win.height();
+	    var bounds = this.offset();
+	    bounds.right = bounds.left + this.outerWidth();
+	    bounds.bottom = bounds.top + this.outerHeight();
+	    return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
+	  };
   });
 </script>
