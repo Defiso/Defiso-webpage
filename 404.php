17d28fb5a6188c9e335dcae781f732b799c4583f
Anton Kjörck Lindén <anton@noviumdesign.se>
2016-08-24 14:50:48 +0200

    Added 404 design

--- 404.php
+++ 404.php
@@ -14,45 +14,19 @@ get_header(); ?>
 
 			<section class="error-404 not-found">
 				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'defisomedia' ); ?></h1>
+					<div class="jumbotron">
+						<div class="inner">
+							<div class="content">
+								<h1 class="page-title"><?php esc_html_e( 'Oops! Sidan kunde inte hittas', 'defisomedia' ); ?></h1>
+							</div>
+						</div>
+					</div>
 				</header><!-- .page-header -->
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'defisomedia' ); ?></p>
+				<div class="entry-content">
+					<h2><?php esc_html_e( 'Sidan kunden inte hittas', 'defisomedia' ); ?></h2>
+					<p>Du kan gå tillbaka till startsidan som du hittar <a href="/">här</a></p>
 
-					<?php
-						get_search_form();
-
-						the_widget( 'WP_Widget_Recent_Posts' );
-
-						// Only show the widget if site has multiple categories.
-						if ( defisomedia_categorized_blog() ) :
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'defisomedia' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-						endif;
-
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'defisomedia' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
 
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
